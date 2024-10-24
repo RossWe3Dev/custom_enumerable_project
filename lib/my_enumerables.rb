2d5d0d3c416cf3692a3c51a801f3d6ea4078e495
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-10-24 19:33:15 +0200

    Implement my_each method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,10 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for element in self do
+      yield(element)
+    end
+    return self
+  end
 end
