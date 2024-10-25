720edfff0c6d0e30a29d84b76d4f006428620a11
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-10-25 12:44:50 +0200

    Clean the code

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -4,7 +4,6 @@ module Enumerable
     self.my_each do |element|
       result.push(element) if yield(element)
     end
-
     result
   end
 
@@ -29,7 +28,7 @@ module Enumerable
     unless block_given?
       # should also check for argument 'pattern' with no block given
 
-      # will work as long as my_all? implements 'pattern' matching
+      # will work as long as my_all? implements pattern matching
       return true unless self.my_all?(nil||false)
       false
     else
@@ -44,7 +43,7 @@ module Enumerable
   def my_none?
     # should check for no armgument and no block, 'pattern' and no block
     
-    # relevant code only for the purpose of the exercise
+    # relevant code for the purpose of the exercise
     self.my_each do |element|
       return false if yield(element)
     end
@@ -53,9 +52,9 @@ module Enumerable
 
   def my_count
     return self.length unless block_given?
-    # should check for 'object' matching
+
+    # should also check for 'object' matching
     
-    # relevant code for the purpose of the exercise
     count = 0
     self.my_each do |element|
       count +=1 if yield(element)
@@ -68,7 +67,6 @@ module Enumerable
     self.my_each do |element|
       result.push(yield(element))
     end
-
     result
   end
 
@@ -81,19 +79,19 @@ module Enumerable
 
 end
 
-# define my_each to iterate trough arrays
+# define #my_each and #my_each_with_index to iterate trough arrays
 class Array
   def my_each
     for element in self do
       yield(element)
     end
-    return self
+    self
   end
 
   def my_each_with_index
     for i in 0...self.length do
       yield(self[i], i)
     end
-    return self
+    self
   end
 end
