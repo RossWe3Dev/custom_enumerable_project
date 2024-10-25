47aebf05fa059ff71a1853d062aa53aaf4dd8b7a
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-10-25 10:37:29 +0200

    Implement #my_any?, comment future commits
    Future commits will include # for methods.
    Last commit should've included '?' for #my_all?

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -22,6 +22,23 @@ module Enumerable
       true
     end
   end
+
+  def my_any?
+    return false if self.empty? && !block_given?
+    unless block_given?
+      # should also check for argument 'pattern' with no block given
+
+      # will work as long as my_all? implements 'pattern' matching
+      return true unless self.my_all?(nil||false)
+      false
+    else
+      self.my_each do |element|
+        return true if yield(element)
+      end
+      false
+    end
+  end
+
 end
 
 # define my_each to iterate trough arrays
